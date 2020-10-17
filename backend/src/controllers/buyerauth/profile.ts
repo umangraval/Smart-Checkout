d33a39c0446f496f19d03d07c4d8e87fd05d0f50
umangraval <umangraval2000@gmail.com>
2020-10-18 01:21:18 +0530

    fix: profile json sanitize

--- backend/src/controllers/buyerauth/profile.ts
+++ backend/src/controllers/buyerauth/profile.ts
@@ -1,10 +1,13 @@
+/* eslint-disable no-underscore-dangle */
 import { RequestHandler } from 'express';
 import requestMiddleware from '../../middleware/request-middleware';
 import Buyer from '../../models/BuyerUser';
 
 const profile: RequestHandler = async (req, res) => {
   const { id } = req.params;
-  const userProfile = await Buyer.findById(id);
+  const userProfile = await Buyer.findById(id, {
+    _id: 0, password: 0, __v: 0
+  });
   res.send({ userProfile });
 };
 
