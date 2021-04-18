911baf5d1389f17170fd75811df31dd7d758d5e2
Mihir <pavuskarmihir@gmail.com>
2021-04-18 11:53:50 +0530

    face mask detection added.

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response
+import facemask_detection
 import cv2
 import threading
+
 app = Flask(__name__)
 
 # initialize a lock used to ensure thread-safe
@@ -13,43 +15,40 @@ lock = threading.Lock()
 #     """Video streaming home page."""
 #     return render_template('index.html')
 
+def generate(mode='None'):
+    global lock
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        with lock:
+            rval, frame = vc.read()
+            if frame is None:
+                continue
+            
+            if mode=='mask':
+                (locs, preds) = facemask_detection.detect_and_predict_mask(frame)
+                frame = facemask_detection.draw_bounding_boxes(locs, preds, frame)
+
+            (flag, encodedImage) = cv2.imencode(".jpg", frame)
+
+            if not flag:
+                continue
+
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+    vc.release()
+
 @app.route('/stream')
 def stream():
    return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
-def generate():
-   # grab global references to the lock variable
-   global lock
-   # initialize the video stream
-   vc = cv2.VideoCapture(0)
-   
-   # check camera is open
-   if vc.isOpened():
-      rval, frame = vc.read()
-   else:
-      rval = False
-
-   # while streaming
-   while rval:
-      # wait until the lock is acquired
-      with lock:
-         # read next frame
-         rval, frame = vc.read()
-         # if blank frame
-         if frame is None:
-            continue
-
-         # encode the frame in JPEG format
-         (flag, encodedImage) = cv2.imencode(".jpg", frame)
-
-         # ensure the frame was successfully encoded
-         if not flag:
-            continue
-
-      # yield the output frame in the byte format
-      yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-   # release the camera
-   vc.release()
+@app.route('/stream_mask')
+def stream_mask():
+    return Response(generate('mask'), mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == '__main__':
    host = "127.0.0.1"
--- react-frontend/src/pages/stream/Monitor.jsx
+++ react-frontend/src/pages/stream/Monitor.jsx
@@ -7,7 +7,7 @@ function Monitor() {
       <h1 className="pageHeader">Monitor</h1>
       <div className='feed'>
         <img 
-          src={'http://127.0.0.1:5000/stream'}
+          src={'http://127.0.0.1:5000/stream_mask'}
           onError={"this.onerror=null; this.src='VideoUnavailable.png';"} 
           className='stream' alt="logo" />
       </div>
