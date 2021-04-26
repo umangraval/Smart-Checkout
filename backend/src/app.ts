ccdcb04e68ba76d77ba91ed143750a346f09a4e6
devamtrivedi <devamtrivedi71@gmail.com>
2021-04-26 11:01:07 +0530

    app and routes blueprint

---
+++ backend/src/app.ts
@@ -0,0 +1,56 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+import path from 'path';
+import express, { Request, Response, NextFunction } from 'express';
+import ApplicationError from './errors/application-error';
+import routes from './routes';
+
+const app = express();
+
+// options for cors midddleware
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'x-auth-token'
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: 'http://localhost:3000',
+  preflightContinue: false
+};
+
+app.use(cors(options));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+app.get('/', (req, res) => {
+  res.redirect('/api/v1/dev/api-docs');
+});
+
+app.get('/test', (req, res) => {
+  res.send('Test working');
+});
+
+app.use('/v1', routes);
+
+app.use((err: ApplicationError, req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  return res.status(err.status || 500).json({
+    error: process.env.NODE_ENV === 'development' ? err : undefined,
+    message: err.message
+  });
+});
+
+export default app;
---
+++ backend/src/routes.ts
@@ -0,0 +1,66 @@
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import apiSpec from '../openapi.json';
+import { checkJwt } from './middleware/checkJwt';
+import { checkRole } from './middleware/checkRole';
+
+import * as AuthController from './controllers/auth';
+import * as BuyerController from './controllers/buyerauth';
+import * as ProductController from './controllers/product';
+import * as CategoryController from './controllers/category';
+import * as TransactionController from './controllers/transaction';
+import * as AnalyticsController from './controllers/analytics';
+import * as QrcodeController from './controllers/qrcode';
+
+const swaggerUiOptions = {
+  customCss: '.swagger-ui .topbar { display: none }'
+};
+
+const router = Router();
+
+// user
+router.post('/auth/add', AuthController.add);
+router.post('/auth/login', AuthController.login);
+router.get('/auth/user/:id', checkJwt, checkRole(['SELLER']), AuthController.profile);
+
+// buyer
+router.post('/buyer/add', BuyerController.add);
+router.post('/buyer/login', BuyerController.login);
+router.get('/buyer/user/:id', checkJwt, checkRole(['BUYER']), BuyerController.profile);
+router.get('/buyer/transaction/:id', checkJwt, checkRole(['BUYER']), TransactionController.buyerTrans);
+
+// product routes
+router.post('/product/add', checkJwt, checkRole(['SELLER']), ProductController.add);
+router.get('/product/all/:id', checkJwt, checkRole(['SELLER']), ProductController.all);
+router.get('/product/search', checkJwt, checkRole(['SELLER']), ProductController.search);
+router.put('/product/update/:id', checkJwt, checkRole(['SELLER']), ProductController.update);
+router.delete('/product/delete/:id', checkJwt, checkRole(['SELLER']), ProductController.del);
+
+// category routes
+router.post('/category/add', checkJwt, checkRole(['SELLER']), CategoryController.add);
+router.get('/category/all/:id', checkJwt, checkRole(['SELLER']), CategoryController.all);
+router.put('/category/update', checkJwt, checkRole(['SELLER']), CategoryController.update);
+router.delete('/category/delete/:id', checkJwt, checkRole(['SELLER']), CategoryController.del);
+
+// transaction routes
+router.post('/transaction/add', checkJwt, checkRole(['BUYER']), TransactionController.add);
+router.get('/transaction/all/:id', checkJwt, checkRole(['SELLER']), TransactionController.all);
+
+// analytics
+router.get('/analytics/pcount/:id', checkJwt, checkRole(['SELLER']), AnalyticsController.productCount);
+router.get('/analytics/ccount/:id', checkJwt, checkRole(['SELLER']), AnalyticsController.categoryCount);
+router.get('/analytics/sale/:id', checkJwt, checkRole(['SELLER']), AnalyticsController.sale);
+router.get('/analytics/daily/:id', checkJwt, checkRole(['SELLER']), AnalyticsController.dailySale);
+router.get('/analytics/customers/:id', checkJwt, checkRole(['SELLER']), AnalyticsController.customerCount);
+router.get('/analytics/outofstock/:id', checkJwt, checkRole(['SELLER']), AnalyticsController.outOfstock);
+
+// qrcode
+router.get('/qrcode/:id', checkJwt, checkRole(['BUYER']), QrcodeController.details);
+
+// Dev routes
+// if (process.env.NODE_ENV === 'development') {
+router.use('/dev/api-docs', swaggerUi.serve);
+router.get('/dev/api-docs', swaggerUi.setup(apiSpec, swaggerUiOptions));
+// }
+
+export default router;
