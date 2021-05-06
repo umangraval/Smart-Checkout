145e36ed045b7562eb4b735b5c5c643d2929bd9f
devamtrivedi <devamtrivedi71@gmail.com>
2021-05-06 20:19:05 +0530

    added comments

--- flask-backend/app/predictions.py
+++ flask-backend/app/predictions.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta, date
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 import keras
+import numpy as np
 
 
 @app.route("/ping")
@@ -19,6 +20,8 @@ def revenePrediction():
 
 @app.route("/predictions/sales")
 def salesPrediction():
+
+    # pre processing data
     df_sales = pd.read_csv(
         '/home/devam/projects/CSE3999-TARP/flask-backend/datasets/sales_prediction_train.csv')
     # df_sales['date'] = df_sales['date'].dt.year.astype(
@@ -50,10 +53,41 @@ def salesPrediction():
 
     X_test, Y_test = test_set_scaled[:, 1:], test_set_scaled[:, 0:1]
     X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+
+    # your project folder
     filename = "/home/devam/projects/CSE3999-TARP/flask-backend/models/sales_prediction"
+
+    # loading model
     loaded_model = keras.models.load_model(filename)
-    result = loaded_model.predict(X_test, batch_size=1)
-    return str(result)
+
+    # predicted values
+    y_pred = loaded_model.predict(X_test, batch_size=1)
+    y_pred = y_pred.reshape(y_pred.shape[0], 1, y_pred.shape[1])
+
+    # post processing
+    pred_test_set = []
+    for index in range(0, len(y_pred)):
+        # print(np.concatenate([y_pred[index], X_test[index]], axis=1))
+        pred_test_set.append(np.concatenate(
+            [y_pred[index], X_test[index]], axis=1))
+    pred_test_set[0]
+    pred_test_set = np.array(pred_test_set)
+    pred_test_set = pred_test_set.reshape(
+        pred_test_set.shape[0], pred_test_set.shape[2])
+    pred_test_set_inverted = scaler.inverse_transform(pred_test_set)
+    result_list = []
+    sales_dates = list(df_sales[-7:].date)
+    act_sales = list(df_sales[-7:].sales)
+
+    for index in range(0, len(pred_test_set_inverted)):
+        result_dict = {}
+        result_dict['pred_value'] = int(
+            pred_test_set_inverted[index][0] + act_sales[index])
+        result_dict['date'] = sales_dates[index+1]
+        result_list.append(result_dict)
+    df_result = pd.DataFrame(result_list)
+
+    return str(df_result)
 
 
 @app.route("/predictions/customers")
